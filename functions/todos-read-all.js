925cbbc96f2f2435d204a1fc27854fed948ceafc
Jahziel Villasana <jahzielv@gmail.com>
2020-02-04 18:31:13 -0500

    changing env vars

--- functions/todos-read-all.js
+++ functions/todos-read-all.js
@@ -2,7 +2,7 @@ const faunadb = require("faunadb");
 
 const q = faunadb.query;
 const client = new faunadb.Client({
-    secret: process.env.FAUNADB_SECRET
+    secret: process.env.FAUNADB_SERVER_SECRET
 });
 
 exports.handler = (event, context, callback) => {
