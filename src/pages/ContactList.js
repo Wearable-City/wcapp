a5285fef9fd1b16b06ab9eed0bd23e7b55a1effd
emanuallan <allanserna@gatech.edu>
2020-04-12 10:36:33 -0500

    allan changes

---
+++ src/pages/ContactList.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import { Table, Input, Typography, Popconfirm, notification, Tag, Spin, message } from 'antd';
+// import '../App.css';
+
+const { Title } = Typography;
+const Search = Input.Search;
+
+// const deleteNotification = (contact_id) => {
+//     message.warning('Contact, ' + contact_id + ' has been deleted');
+// };
+
+// const editNotification = (contact_id) => {
+//     message.warning('Contact, ' + contact_id + ' has been updated');
+// };
+
+// const createNotification = (contact_id) => {
+//     message.warning('Contact, ' + contact_id + ' has been added');
+// };
+
+class ContactList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            output: undefined,
+            data: undefined,
+            ringId: undefined,
+            userName: undefined,
+            firstName: undefined,
+            lastName: undefined,
+            password: undefined,
+            contacts: undefined
+        };
+
+        this.columns = [
+            {
+                title: 'Contact First Name',
+                dataIndex: 'firstName',
+                key: 'contact_id',
+
+            },
+
+            {
+                title: 'Contact First Name',
+                dataIndex: 'lastName',
+                key: 'contact_id',
+
+            },
+
+            {
+                title: 'Contact Phone Number',
+                dataIndex: 'phoneNumber',
+                key: 'contact_id',
+
+            },
+            {
+                title: 'Alert Message',
+                dataIndex: 'alertMessage',
+                key: 'contact_id',
+
+            }
+        ]
+    }
+
+    fetchData = () => {
+        fetch(
+            "https://wearablecity.netlify.com/.netlify/functions/users-read-by-ringid?ringid=42069",
+            { mode: "cors" }
+        )
+            .then(response => response.json())
+            .then(output =>
+                this.setState({
+                    output: output,
+                    data: output[0].data,
+                    ringId: output[0].data.ringId,
+                    userName: output[0].data.userName,
+                    firstName: output[0].data.firstName,
+                    lastName: output[0].data.lastName,
+                    password: output[0].data.password,
+                    contacts: output[0].data.contacts
+                })
+            );
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    handleSelect = (contact_id) => {
+        this.setState({ selectContact: contact_id })
+    }
+
+    render() {
+        // if (!this.state.loaded) {
+        //     for (let i = 0; i < this.state.contacts.length; i++) {
+        //         this.state.contactsOnDisplay.push(this.state.contacts[i]);
+        //         this.state.contacts.push(this.state.contacts[i])
+        //     }
+
+        //     this.setState({ loaded: true })
+        // }
+
+        return (
+            <div>
+                <div>
+                    <Title level={2} style={{ float: "left", color: "white" }}> ContactList </Title>
+                    <div style={{ textAlign: "right" }}>
+                        <Search
+                            placeholder="Search"
+                            id="search_contact"
+                            onChange={this.handleChange}
+                            style={{ width: 215 }}
+                        />
+                    </div>
+                </div>
+                <Table
+                    columns={this.columns}
+                    loading={!this.state.loaded}
+                    dataSource={this.state.contacts}
+                />
+            </div>
+        );
+    }
+}
+
+export default ContactList;
