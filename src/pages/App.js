60f90f5a474eb5b8255004a803d02412bd588457
Jahziel Villasana <jahzielv@gmail.com>
2020-04-13 22:38:39 -0400

    auth works

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,18 +3,26 @@ import "../App.css";
 import HomePage from "./HomePage";
 import ContactList from "./ContactList";
 import AuthPage from "./AuthPage";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+const GoToAuth = () => <Redirect to="/auth" />;
 
 function App() {
     return (
         <div className="App">
             <Switch>
+                <Route path="/auth" component={AuthPage} />
                 <Route
                     path="/"
-                    component={localStorage.getItem("auth_token") ? HomePage : AuthPage}
+                    component={localStorage.getItem("auth_token") ? HomePage : GoToAuth}
                     exact
                 />
-                <Route path="/settings" component={ContactList} />
+                <Route
+                    path="/settings"
+                    component={
+                        localStorage.getItem("auth_token") ? ContactList : GoToAuth
+                    }
+                />
             </Switch>
         </div>
     );
--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,6 +1,6 @@
-import React from "react";
-
-import { Form, Input, Button, Checkbox } from "antd";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { Form, Input, Button } from "antd";
 
 const layout = {
     labelCol: {
@@ -20,14 +20,25 @@ const tailLayout = {
 const AUTH_URL = "https://wearablecity.netlify.com/.netlify/functions/auth";
 
 const AuthForm = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const onFinish = (values) => {
-        console.log("Success:", values);
-        fetch(AUTH_URL, { mode: "cors", body: values, method: "POST" })
+        console.log("Success:", {
+            userName: values.userName,
+            password: values.password,
+        });
+        fetch(AUTH_URL, {
+            mode: "cors",
+            body: JSON.stringify({
+                userName: values.userName,
+                password: values.password,
+            }),
+            method: "POST",
+        })
             .then((res) => res.json())
             .then((data) => {
                 localStorage.setItem("auth_token", data.token);
                 //navigate to homepage});
-                console.log("yee");
+                setIsLoggedIn(true);
             });
     };
 
@@ -35,53 +46,53 @@ const AuthForm = () => {
         console.log("Failed:", errorInfo);
     };
 
-    return (
-        <Form
-            {...layout}
-            name="basic"
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-        >
-            <Form.Item
-                label="Username"
-                name="userName"
-                rules={[
-                    {
-                        required: true,
-                        message: "Please input your username!",
-                    },
-                ]}
+    if (!isLoggedIn) {
+        return (
+            <Form
+                {...layout}
+                name="basic"
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
             >
-                <Input />
-            </Form.Item>
+                <Form.Item
+                    label="Username"
+                    name="userName"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your username!",
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
 
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: "Please input your password!",
-                    },
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your password!",
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
 
-            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-                <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
-    );
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        );
+    } else {
+        return <Redirect to="/" />;
+    }
 };
 
 class AuthPage extends React.Component {
