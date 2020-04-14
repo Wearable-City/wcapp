f6692704f15783c303124c466a0679734a3315dc
Jahziel Villasana <jahzielv@gmail.com>
2020-04-13 21:22:55 -0400

    basic auth flow skeleton, need auth route

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import "../App.css";
 import HomePage from "./HomePage";
 import ContactList from "./ContactList";
+import AuthPage from "./AuthPage";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <Switch>
-                <Route path="/" component={HomePage} exact />
+                <Route
+                    path="/"
+                    component={localStorage.getItem("auth_token") ? HomePage : AuthPage}
+                    exact
+                />
                 <Route path="/settings" component={ContactList} />
             </Switch>
         </div>
---
+++ src/pages/AuthPage.js
@@ -0,0 +1,111 @@
+import React from "react";
+
+import { Form, Input, Button, Checkbox } from "antd";
+
+const layout = {
+    labelCol: {
+        span: 8,
+    },
+    wrapperCol: {
+        span: 16,
+    },
+};
+const tailLayout = {
+    wrapperCol: {
+        offset: 8,
+        span: 16,
+    },
+};
+
+const AUTH_URL = "";
+
+const AuthForm = () => {
+    const onFinish = (values) => {
+        console.log("Success:", values);
+        fetch(AUTH_URL, { mode: "cors", body: values, method: "POST" })
+            .then((res) => res.json())
+            .then((data) => {localStorage.setItem("auth_token", data.token); //navigate to homepage});
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log("Failed:", errorInfo);
+    };
+
+    return (
+        <Form
+            {...layout}
+            name="basic"
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input your username!",
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input your password!",
+                    },
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+class AuthPage extends React.Component {
+    render() {
+        return (
+            <div
+                style={{
+                    display: "flex",
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100%",
+                    width: "100%",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        justifyItems: "center",
+                    }}
+                >
+                    <AuthForm />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AuthPage;
