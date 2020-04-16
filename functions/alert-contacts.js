391d23c8e0248bd0720a73f9c2be272269d114aa
Jahziel Villasana <jahzielv@gmail.com>
2020-04-15 21:57:57 -0400

    location added to alert api

--- functions/alert-contacts.js
+++ functions/alert-contacts.js
@@ -14,11 +14,11 @@ const TEST_NUM = "+14044287664";
  * Texts an array of contacts
  * @param {Array<Object>} contacts
  */
-function textContacts(contacts) {
+function textContacts(contacts, location) {
     contacts.forEach((e) => {
         twilioClient.messages
             .create({
-                body: e.alertMessage,
+                body: `${e.alertMessage} location: ${JSON.parse(location)}`,
                 from: TWILIO_PHONE_NUM,
                 to: e.phoneNumber,
             })
@@ -29,6 +29,18 @@ function textContacts(contacts) {
 exports.handler = (event, context, callback) => {
     console.log("Function `alert-contacts` invoked");
     console.log(event["queryStringParameters"]["ringid"]);
+    let locData;
+    if (
+        event["queryStringParameters"]["lat"] &&
+        event["queryStringParameters"]["long"]
+    ) {
+        locData = {
+            long: event["queryStringParameters"]["lat"],
+            lat: event["queryStringParameters"]["long"],
+        };
+    } else {
+        locData = { long: 0, lat: 0 };
+    }
     return client
         .query(
             q.Paginate(
@@ -53,7 +65,7 @@ exports.handler = (event, context, callback) => {
                 // ret[0].data.contacts.forEach((contact) => {
                 //     console.log(`Calling twilio for ${contact.phoneNumber}`);
                 // });
-                textContacts(ret[0].data.contacts);
+                textContacts(ret[0].data.contacts, locData);
                 return callback(null, {
                     statusCode: 200,
                     body: JSON.stringify(ret),
